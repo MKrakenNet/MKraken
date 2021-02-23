525793ff29d36b1d0e1c34bf1c8d98bd7e3f133a
Frank Weller <github@iforge.de>
2021-02-23 14:52:30 +0100

    DomainRoutingAdded

---
+++ src/MKraken/Domain/Routing/IRouteManager.cs
@@ -0,0 +1,11 @@
+﻿namespace MKraken.Domain.Routing
+{
+    public interface IRouteManager
+    {
+        string GetRouteTo(string internalRoute);
+
+        string GetRouteTo(string internalRoute, string internalReturnRoute);
+
+        void NavigateTo(string internalRoute);
+    }
+}
---
+++ src/MKraken/Domain/Routing/RouteManager.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace MKraken.Domain.Routing
+{
+    public class RouteManager : IRouteManager
+    {
+        public RouteManager(NavigationManager navigationManager)
+        {
+            this.NavigationManager = navigationManager ?? throw new ArgumentNullException(nameof(navigationManager));
+        }
+
+        public NavigationManager NavigationManager { get; }
+
+        public string GetRouteTo(string internalRoute)
+        {
+            return $"{this.NavigationManager.BaseUri}{internalRoute.TrimStart('/')}";
+        }
+
+        public string GetRouteTo(string internalRoute, string internalReturnRoute)
+        {
+            return $"{this.GetRouteTo(internalRoute)}?ReturnUrl={internalReturnRoute}";
+        }
+
+        public void NavigateTo(string internalRoute)
+        {
+            if (internalRoute.StartsWith(this.NavigationManager.BaseUri, StringComparison.InvariantCultureIgnoreCase))
+            {
+                this.NavigationManager.NavigateTo(this.GetRouteTo(internalRoute.Remove(0, this.NavigationManager.BaseUri.Length - 1)));
+            }
+            else
+            {
+                this.NavigationManager.NavigateTo(this.GetRouteTo(internalRoute));
+            }
+        }
+    }
+}
